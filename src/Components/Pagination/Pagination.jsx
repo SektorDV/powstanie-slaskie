03647fc531c4766e78c5a5a15578613846fa8d4e
Koszewski Gustaw <koszewski@prsa.pl>
2019-08-01 11:20:07 +0200

    pagination wip

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import {BrowserRouter, Link } from 'react-router-dom';
 import './Pagination.scss';
 
 const Pagination = () => {
     return (
-        <div class="ps__pagination">
-            <span className="pagination__dot pagination__dot--active"></span>
-            <span className="pagination__dot"></span>
-            <span className="pagination__dot"></span>
-            <span className="pagination__dot"></span>
-            <span className="pagination__dot"></span>
-            <span className="pagination__dot"></span>
-            <span className="pagination__dot"></span>
-            <span className="pagination__dot"></span>
+        <div className="ps__pagination">
+            <ul className="pagination__list">
+                <Link to={`/`}><li className="pagination__link"></li></Link>
+                <Link to={`/PoWojnie`}><li className="pagination__link"></li></Link>
+                <Link to={`/Korfanty`}><li className="pagination__link"></li></Link>
+                <Link to={`/IPowstanie`}><li className="pagination__link"></li></Link>
+                <Link to={`/IIPowstanie`}><li className="pagination__link"></li></Link>
+                <Link to={`/Plebiscyt`}><li className="pagination__link"></li></Link>
+                <Link to={`/IIIPowstanie`}><li className="pagination__link"></li></Link>
+                <Link to={`/Skutki`}><li className="pagination__link"></li></Link>
+            </ul>
         </div>
     )
 }
