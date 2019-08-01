170c669fa7f012ec5f7ad8aeab4139e573cc3b97
Romańczuk Michał <romanczuk@prsa.pl>
2019-08-01 14:36:00 +0200

    initial page template

--- src/Views/PoWojnie/PoWojnie.jsx
+++ src/Views/PoWojnie/PoWojnie.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './PoWojnie.scss';
 import Pagination from '../../Components/Pagination/Pagination';
+import Page from '../../Components/Page/Page';
+import Background from '../../res/images/bg/powojniebg.png';
+
+const content = {
+    headline: `"Wszystkie dążenia zmierzające do oderwania Górnego Śląska od Rzeszy Niemieckiej stanowią przestępstwo zdrady stanu."`,
+    subheadline: `z rozporządzenia Walthera von Miguela, 31 grudnia 1918`,
+    bg: Background
+}
+
 
 const PoWojnie = () => {
     return(
-        <div className="ps__powojnie">
-            PO WOJNIE</div>
+        <Page content={content}></Page>
     )
 }
 
