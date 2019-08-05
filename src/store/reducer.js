1a75540632a1c777f0abb1b4e1ea886409fecfc7
Romańczuk Michał <romanczuk@prsa.pl>
2019-08-05 10:08:17 +0200

    initial modal dispatchers for video, audio and map

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,19 @@
 const initialState = {
-    dummy: true
+    showModal: false
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case 'SWITCH_ON_MODAL':
+            return {
+                ...state,
+                showModal: true
+            }
+        case 'SWITCH_OFF_MODAL':
+            return {
+                ...state,
+                showModal: false
+            }
         default:
             return state;
     }
