0ec9037564019c52f44b0a6879765a1a1fdb9ba9
Romańczuk Michał <romanczuk@prsa.pl>
2019-08-12 11:43:37 +0200

    page grid layout, deoptimized bgs

--- src/Components/Page/Page.jsx
+++ src/Components/Page/Page.jsx
@@ -1,42 +1,52 @@
 import React, { useState, useEffect } from "react";
 import "./Page.scss";
-import Buttons from '../Buttons/Buttons';
-import Textfield from '../Textfield/Textfield';
-import {connect, dispatch} from 'react-redux';
-import {parallax} from '../../functions/parallax';
-
+import Buttons from "../Buttons/Buttons";
+import Textfield from "../Textfield/Textfield";
+import { connect, dispatch } from "react-redux";
+import { parallax } from "../../functions/parallax";
 
 const Page = props => {
   const content = [];
-  
+
   const audioContent = [];
-  props.content.multimediaItems.forEach(el => el.type === "audio" ? audioContent.push(el) : false);
+  props.content.multimediaItems.forEach(el =>
+    el.type === "audio" ? audioContent.push(el) : false
+  );
 
   const videoContent = [];
-  props.content.multimediaItems.forEach(el => el.type === "video" ? videoContent.push(el) : false);
+  props.content.multimediaItems.forEach(el =>
+    el.type === "video" ? videoContent.push(el) : false
+  );
 
   const mapContent = [];
-  props.content.multimediaItems.forEach(el => el.type === "map" ? mapContent.push(el) : false);
+  props.content.multimediaItems.forEach(el =>
+    el.type === "map" ? mapContent.push(el) : false
+  );
 
   props.content.menuItems.forEach(e => {
     if (e.content != "") content.push(e.content);
   });
   const [menuSelection, setMenuSelection] = useState(0);
-  const [imgStyle, setImgStyle] = useState({X:0,Y:0})
+  const [imgStyle, setImgStyle] = useState({ X: 0, Y: 0 });
   const [x, updatePositionX] = useState();
 
-  useEffect(()=>{
-    parallax(x, setImgStyle)
-  
+  useEffect(() => {
+    parallax(x, setImgStyle);
   }, [x]);
 
   return (
-    <div className="ps__page__main"  onMouseMove={e=>updatePositionX(e.clientX)}>
+    <div
+      className="ps__page__main"
+      onMouseMove={e => updatePositionX(e.clientX)}
+    >
       <div
-        className={`ps__page__left ${props.content.titleLeft ? '' : 'right-align' }`}
+        className={`ps__page__left ${
+          props.content.titleLeft ? "" : "right-align"
+        }`}
         style={{
-          backgroundImage: `url(${props.content.bg})`, backgroundPositionX:-142 + imgStyle.X+'px',
-          backgroundPositionY:0 + imgStyle.Y+'px'
+          backgroundImage: `url(${props.content.bg})`,
+          backgroundPositionX: -142 + imgStyle.X + "px",
+          backgroundPositionY: 0 + imgStyle.Y + "px"
         }}
       >
         <div className="ps__page__left__title">
@@ -53,45 +63,52 @@ const Page = props => {
         </div>
       </div>
       <div className="ps__page__right">
-        <div className="ps__page__right__top">
-          <div className="ps__page__right__top__left">
-            <div className="ps__page__right__top__left__menu">
-              {props.content.menuItems.map((e, index) => {
-                return (
-                  <div
-                    key={index}
-                    onClick={() => {
-                      if (e.type === "text") setMenuSelection(index);
-                      else {
-                        props.sendDataToModal(e.content.img||null, e.content.author||null, e.content.content||null, e.type, e.content.modalTabs||null)
-                        props.showModal();
-                      }
-                      
-                    }}
-                    style={menuSelection === index ? { color: "black" } : null}
-                  >
-                    {e.label}
-                  </div>
-                );
-              })}
+        <div className="ps__page__right__menu">
+          <div className="ps__page__right__menu__top">
+            {props.content.menuItems.map((e, index) => {
+              return (
+                <div
+                  key={index}
+                  onClick={() => {
+                    if (e.type === "text") setMenuSelection(index);
+                    else {
+                      props.sendDataToModal(
+                        e.content.img || null,
+                        e.content.author || null,
+                        e.content.content || null,
+                        e.type,
+                        e.content.modalTabs || null
+                      );
+                      props.showModal();
+                    }
+                  }}
+                  style={menuSelection === index ? { color: "black" } : null}
+                >
+                  {e.label}
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="ps__page__right__menu__icons">
+            <div className="ps__page__right__menu__icons__label">
+              <span>POSŁUCHAJ I ZOBACZ</span>
             </div>
-            <div className="ps__page__right__top__left__icons">
-              <div className="ps__page__right__top__left__icons__label"><span>POSŁUCHAJ I ZOBACZ</span></div>
-              <Buttons 
-              audio={audioContent.length > 0} 
-              audioContent={audioContent} 
-              video={videoContent.length > 0} 
+            <Buttons
+              audio={audioContent.length > 0}
+              audioContent={audioContent}
+              video={videoContent.length > 0}
               videoContent={videoContent}
               map={mapContent.length > 0}
-              mapContent={mapContent} 
-              />
-            </div>
+              mapContent={mapContent}
+            />
           </div>
-          <div
-            className="ps__page__right__top__right"
-            style={{ backgroundImage: `url(${props.content.smallImg})` }}
-          />
         </div>
+
+        <div
+          className="ps__page__right__imgSmall"
+          style={{ backgroundImage: `url(${props.content.smallImg})` }}
+        />
         <div className="ps__page__right__bottom">
           <Textfield content={content[menuSelection]} />
         </div>
@@ -100,11 +117,11 @@ const Page = props => {
   );
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     sendDataToModal: (img, imgAuthor, content, type, modalTabs) => {
       dispatch({
-        type: 'SET_MODAL_DATA',
+        type: "SET_MODAL_DATA",
         payload: {
           img: img,
           imgAuthor: imgAuthor,
@@ -112,10 +129,13 @@ const mapDispatchToProps = (dispatch) => {
           type: type,
           modalTabs: modalTabs
         }
-      })
+      });
     },
-    showModal: () => dispatch({type: 'SWITCH_ON_MODAL'})
-  }
-}
+    showModal: () => dispatch({ type: "SWITCH_ON_MODAL" })
+  };
+};
 
-export default connect(null, mapDispatchToProps)(Page);
+export default connect(
+  null,
+  mapDispatchToProps
+)(Page);
