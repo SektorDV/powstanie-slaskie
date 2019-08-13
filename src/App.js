e97afbb9b0472619da0b0ec5e923b65b5a0f0c36
Romańczuk Michał <romanczuk@prsa.pl>
2019-08-13 15:05:52 +0200

    domain name fix

--- src/App.js
+++ src/App.js
@@ -34,16 +34,14 @@ const App = withRouter(({location, ...props}) => {
         <CSSTransition key={location.key} classNames='fade' timeout={{enter: 600, exit: 300}}>
         <section className="route-section">
           <Switch location={location}>
-            <Route path="/powstanieslaskie/" component={Intro} />
-            <Route path="/" exact component={Intro} />
-            <Route path="" exact component={Intro} />
-            <Route path="/PoWojnie" exact component={PoWojnie} />
-            <Route path="/Korfanty" exact component={Korfanty} />
-            <Route path="/IPowstanie" exact component={IPowstanie} />
-            <Route path="/IIPowstanie" exact component={IIPowstanie} />
-            <Route path="/Plebiscyt" exact component={Plebiscyt} />
-            <Route path="/IIIPowstanie" exact component={IIIPowstanie} />
-            <Route path="/Skutki" exact component={Skutki} />
+            <Route path="/powstaniaslaskie/" exact component={Intro} />
+            <Route path="/powstaniaslaskie/PoWojnie" exact component={PoWojnie} />
+            <Route path="/powstaniaslaskie/Korfanty" exact component={Korfanty} />
+            <Route path="/powstaniaslaskie/IPowstanie" exact component={IPowstanie} />
+            <Route path="/powstaniaslaskie/IIPowstanie" exact component={IIPowstanie} />
+            <Route path="/powstaniaslaskie/Plebiscyt" exact component={Plebiscyt} />
+            <Route path="/powstaniaslaskie/IIIPowstanie" exact component={IIIPowstanie} />
+            <Route path="/powstaniaslaskie/Skutki" exact component={Skutki} />
           </Switch>
           </section>
         </CSSTransition>
