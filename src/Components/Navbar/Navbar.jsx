7ab7f630cfa95e9c494d21b80b33298a53ba47e0
Romańczuk Michał <romanczuk@prsa.pl>
2019-08-14 11:21:21 +0200

    css fixes, domain fix

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,14 +7,14 @@ console.log(match)
     return(
         <div className="ps__navbar">
             <ul>
-                <NavLink to={`/powstaniaslaskie/`} exact activeClassName="nav_active"><li><div className="nav_select"></div><span>Główna</span></li></NavLink>
-                <NavLink to={`/powstaniaslaskie/PoWojnie`} activeClassName="nav_active"><li><div className="nav_select"></div><span>Śląsk po I Wojnie Światowej</span></li></NavLink>
-                <NavLink to={`/powstaniaslaskie/Korfanty`} activeClassName="nav_active"><li><div className="nav_select"></div><span>Wojciech Korfanty</span></li></NavLink>
-                <NavLink to={`/powstaniaslaskie/IPowstanie`} activeClassName="nav_active"><li><div className="nav_select"></div><span>I Powstanie Śląskie</span></li></NavLink>
-                <NavLink to={`/powstaniaslaskie/IIPowstanie`} activeClassName="nav_active"><li><div className="nav_select"></div><span>II Powstanie Śląskie</span></li></NavLink>
-                <NavLink to={`/powstaniaslaskie/Plebiscyt`} activeClassName="nav_active"><li><div className="nav_select"></div><span>Plebiscyt</span></li></NavLink>
-                <NavLink to={`/powstaniaslaskie/IIIPowstanie`} activeClassName="nav_active"><li><div className="nav_select"></div><span>III Powstanie Śląskie</span></li></NavLink>
-                <NavLink to={`/powstaniaslaskie/Skutki`} activeClassName="nav_active"><li><div className="nav_select"></div><span>Skutki i Ocena</span></li></NavLink>
+                <NavLink to={`/`} exact activeClassName="nav_active"><li><div className="nav_select"></div><span>Główna</span></li></NavLink>
+                <NavLink to={`/PoWojnie`} activeClassName="nav_active"><li><div className="nav_select"></div><span>Śląsk po I Wojnie Światowej</span></li></NavLink>
+                <NavLink to={`/Korfanty`} activeClassName="nav_active"><li><div className="nav_select"></div><span>Wojciech Korfanty</span></li></NavLink>
+                <NavLink to={`/IPowstanie`} activeClassName="nav_active"><li><div className="nav_select"></div><span>I Powstanie Śląskie</span></li></NavLink>
+                <NavLink to={`/IIPowstanie`} activeClassName="nav_active"><li><div className="nav_select"></div><span>II Powstanie Śląskie</span></li></NavLink>
+                <NavLink to={`/Plebiscyt`} activeClassName="nav_active"><li><div className="nav_select"></div><span>Plebiscyt</span></li></NavLink>
+                <NavLink to={`/IIIPowstanie`} activeClassName="nav_active"><li><div className="nav_select"></div><span>III Powstanie Śląskie</span></li></NavLink>
+                <NavLink to={`/Skutki`} activeClassName="nav_active"><li><div className="nav_select"></div><span>Skutki i Ocena</span></li></NavLink>
             </ul>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import 'raf/polyfill';
 import "react-app-polyfill/ie9"
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
-import 'raf/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
